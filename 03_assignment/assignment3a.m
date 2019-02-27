a478b4aa3d1a5a3d2e0a2c0c0209b77b79d6ba4a
Dhiraj Saharia <dsaharia@outlook.com>
2019-02-27 17:30:04 +0530

    assignment 3

--- 03_assignment/assignment3a.m
+++ 03_assignment/assignment3a.m
@@ -1,10 +1,23 @@
 % Sine Signal generation with random noise.
-
+N = 128;
 fs = 8000;
-t = 0: 1/fs : 128/fs-1/fs;
+t = 0: 1/fs : N/fs-1/fs;
 f = 120;
-x1 = sin(2 * pi * f * t);
+x1 = sin(2 * pi * f * t); % Sine signal generation.
 
-noise = randn(1, 128);
-x = x1 + noise;
-plot(x);
+noise_gaussian = randn(1, length(x1)); % generating noise
+noise_uniform = rand(1,length(x1));
+
+gau_signal = x1 + noise_gaussian; % adding the noise
+uni_signal = x1 + noise_uniform;
+
+
+title('Different type of Noise');
+subplot(1,2,1);
+plot(gau_signal);
+grid on;
+title('Signal with Gaussian noise');
+subplot(1,2,2);
+plot(uni_signal);
+grid on;
+title('Signal with Uniform noise');
--- 03_assignment/assignment3b.m
+++ 03_assignment/assignment3b.m
@@ -4,7 +4,7 @@
 %whose magnitude of the sinusoidal components are 40dB and close to 42dB respectively
 fs = 7000;
 N = 256;
-t = 0: 1/fs : 256/fs - 1/fs;
+t = 0: 1/fs : N/fs - 1/fs;
 f1 = 1000;
 f2 = 1500;
 
@@ -12,16 +12,30 @@ s1 = sin(2 * pi * f1 * t);
 s2 = sin(2 * pi * f2 * t);
 
 z1 = fft(s1);
-z2 = fft(s1);
+z2 = fft(s2);
 
-z1_mag = abs(z1);
-z2_mag = abs(z2);
+z1_mag = 20 * log10(abs(z1));
+z2_mag = 20 * log10(abs(z2));
 
 fx = 0: N/2-1;
-fx = (fx*fs)/N;
-hold a
+fx = (fx.*fs)/N;
+
+subplot(2,2,1);
+plot(s1);
+title('Sine Signal with f = 1000Hz');
 grid on;
 
+subplot(2,2,2);
+plot(s2);
+title('Sine Signal with f = 1500Hz')
+grid on;
+
+subplot(2,2,3);
 plot(fx,z1_mag(1:128));
+title('FFT of s1');
+grid on;
+
+subplot(2,2,4);
 plot(fx,z2_mag(1:128));
-hold off;
+title('FFT of s2');
+grid on;
