24a5cd965a53280b77451f6c3dcde79968fdd4c2
Dhiraj Saharia <dsaharia@outlook.com>
2019-05-03 18:10:09 +0530

    removed

--- 06_assignment/1.m
+++
@@ -1,7 +0,0 @@
-% Write a program to demonstrate the DSB-TC AM, i.e. commercial AM implementation for a baseband cosine signal of carrier frequency 15 Hz. 
-% Take a carrier signal whose carrier frequency be 30Hz (i.e. fc for multiplier cosine signal in the formula) and amplitude is 8 samples per carrier. 
-% Now consider the modulation index to be 0.1 and the carrier amplitude of the proposed AM signal be 1.
-% Also consider the baseband and the carrier signal propagates at the rate of 1 milliseconds.
-% Now plot the AM signal. Do remember to plot the carrier and the baseband original signal also.
-
-fc = 15
